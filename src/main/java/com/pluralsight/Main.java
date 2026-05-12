149176a1bce91a8fbc1cc6d56c44ced5300f9875
Shah Ali Omor <omoranam@gmail.com>
2026-05-12 13:33:33 -0400

    Finished part 1 and 2

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,4 +1,27 @@
 package com.pluralsight;
 
 public class Main {
+    public static void main(String[] args) {
+        Moped slowRide= new Moped();
+        slowRide.setColor("Red");
+        slowRide.setCargoCapacity(2);
+        slowRide.setNumberOfPassengers(2);
+        slowRide.setFuelCapacity(5);
+
+        Car familyCar= new Car();
+        familyCar.setColor("white");
+        familyCar.setCargoCapacity(5);
+        familyCar.setNumberOfPassengers(5);
+        familyCar.setFuelCapacity(13);
+
+        SemiTruck truck=new SemiTruck();
+        truck.setCargoCapacity(1000);
+        truck.setColor("black");
+        truck.setNumberOfPassengers(3);
+        truck.setFuelCapacity(20);
+
+        System.out.println(familyCar.getColor());
+
+
+    }
 }
